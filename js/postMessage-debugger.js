ef1e3e178c0e9726612b1f3c1d1bd1a233982cab
danbars <dan@bar-shalom.com>
2018-05-28 18:29:38 +0300

    Easier read of log when there are multiple iframes
    When there are multiple iframes it is sometimes hard to read the log, especially if they don't have title.
    Added id to each frame so you can later reference to it from the log.
    Use hash method so the id is consistent across refresh of the page

--- js/postMessage-debugger.js
+++ js/postMessage-debugger.js
@@ -1,8 +1,12 @@
 var postMessageDebugger = {
+  hashCode: function(s){
+      return s.split("").reduce(function(a,b){a=((a<<5)-a)+b.charCodeAt(0);return a&a},0);              
+  },
   init: function() {
-    console.log("postMessageDebugger activated");
+    var id = this.hashCode(window.location.href);
+    console.log("[" + id + "] postMessageDebugger activated on " + window.location.href);
     addEventListener("message", function(event) {
-      console.log(document.title + " received '" + event.data + "'")
+      console.log("[" + id + "]" + event.data )
     });
   }
 };
